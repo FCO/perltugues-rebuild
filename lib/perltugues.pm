320e429f12636ab674c774c931580e723ee1d8a6
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-06-20 14:52:23 -0300

    usando o Filter::Simple

--- lib/perltugues.pm
+++ lib/perltugues.pm
@@ -4,108 +4,22 @@ use 5.006;
 use strict;
 use warnings;
 
-=head1 NAME
+use Filter::Simple;
+use perltugues::Conterver;
 
-perltugues - The great new perltugues!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
+FILTER {
+   my $class = shift;
+   my %par   = @_;
 
 
-=head1 SYNOPSIS
+   $_ = perltugues::Converter->new->convert($_);
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use perltugues;
-
-    my $foo = perltugues->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
+   if($par{ DEBUG }) {
+      Perl::Tidy::perltidy(source => \$_, destination => \$_)
+         if eval "require Perl::Tidy";
+      print;
+      exit
+   }
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Fernando Correa de Oliveira, C<< <fco at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-perltugues at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=perltugues>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc perltugues
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=perltugues>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/perltugues>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/perltugues>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/perltugues/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2012 Fernando Correa de Oliveira.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
-
-1; # End of perltugues
+42
